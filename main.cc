36c7ef1a0ceba323866465382df8df50cfa1022e
Olzhas Kaiyrakhmet <olzhabay.i@gmail.com>
2017-11-13 19:25:20 +0900

    deleted unnecessary files. fixed cmake and pm flush

--- main.cc
+++ main.cc
@@ -4,41 +4,24 @@
 #include "leveldb/table_builder.h"
 
 uint64_t clflush_cnt = 0;
-
-void standard_db_test() {
-  std::cout << "start\n";
-  leveldb::DB* db;
-  leveldb::Options options;
-  options.filter_policy = NULL;
-  options.create_if_missing = true;
-  options.compression = leveldb::kNoCompression;
-  std::string dbpath = "/tmp/testdb";
-  leveldb::Status status = leveldb::DB::Open(options, dbpath, &db);
-  std::cout << "open\n";
-  assert(status.ok());
-  std::string key = "test";
-  std::string val = "test_value";
-  status = db->Put(leveldb::WriteOptions(), key, val);
-  if (status.ok())
-    std::cout << "success put\n";
-  std::string outval;
-  status = db->Get(leveldb::ReadOptions(), key, &outval);
-  if (status.ok())
-    std::cout << "success get " << outval << "\n";
-  std::cout << key << " : " << outval << std::endl;
-}
+uint64_t WRITE_LATENCY_IN_NS = 0;
 
 
-void sst_db_test() {
+int main(int argc, char** argv) {
+  if (argc < 2) {
+    printf("file arg\n");
+    return 1;
+  }
   struct timespec start, end;
-  int numData = 1000000;
+  int numData = 10000000;
   leveldb::DB* db;
   leveldb::Options options;
   options.filter_policy = NULL;
   options.create_if_missing = true;
   options.compression = leveldb::kNoCompression;
-  options.write_buffer_size = 8000;
-  std::string dbpath = "/temp/testdb";
+  options.max_file_size = 8 << 20;
+  char *c = argv[1];
+  std::string dbpath(c);
   leveldb::Status status = leveldb::DB::Open(options, dbpath, &db);
   assert(status.ok());
   clock_gettime(CLOCK_MONOTONIC, &start);
@@ -55,17 +38,16 @@ void sst_db_test() {
 
   clock_gettime(CLOCK_MONOTONIC, &start);
   for (auto i = 0; i < numData; i++) {
+    std::string v = "value" + std::to_string(i);
     std::stringstream key;
     key << "key0key" << i;
     std::string value;
     status = db->Get(leveldb::ReadOptions(), key.str(), &value);
+    if (v.compare(value) != 0) {
+      printf("%s %s %s\n", key.str().data(), v.data(), value.data());
+    }
   }
   clock_gettime(CLOCK_MONOTONIC, &end);
   elapsed = (end.tv_sec - start.tv_sec)*1000000000 + (end.tv_nsec - start.tv_nsec);
   std::cout << elapsed/1000 << "\tusec\t" << (uint64_t)(1000000*(numData/(elapsed/1000.0))) << "\tOps/sec\tRead" << std::endl;
-}
-
-int main(int argc, char** argv) {
-  //standard_db_test();
-  sst_db_test();
 }
